c0b0101e97573f09fd4d135f03f8150a920be655
Reinel Osorio <rosorior@uqvirtual.edu.co>
2026-09-23 21:08:40 -0500

    subiendo cambios

--- src/main/java/model/Academia.java
+++ src/main/java/model/Academia.java
@@ -1,4 +1,10 @@
-package model;
+package co.edu.uniquindio.lenguajecafetero.model;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
 public class Academia {
 }
