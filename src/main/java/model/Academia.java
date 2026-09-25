3ddd6ac51c9881be0a8f9882e8fcd606a73a9a3b
Reinel Osorio <rosorior@uqvirtual.edu.co>
2026-09-24 19:10:18 -0500

    mis cambios

--- src/main/java/model/Academia.java
+++ src/main/java/model/Academia.java
@@ -1,10 +1,26 @@
 package co.edu.uniquindio.lenguajecafetero.model;
-
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
-public class Academia {
-}
+
+
+
+public final class Academia {
+
+    private final String nombreComercial;
+    private final String nit;
+    private final String direccion;
+    private final String telefono;
+    private final String correo;
+    private final String paginaWeb;
+
+    private final List<Estudiante> estudiantes = new ArrayList<>();
+    private final List<Profesor> profesores = new ArrayList<>();
+    private final List<Curso> cursos = new ArrayList<>();
+    private final List<ServicioAdicional> serviciosAdicionales = new ArrayList<>();
+    private final List<Matricula> matriculas = new ArrayList<>();
+    private final List<PoliticaDescuento> politicasDescuento = new ArrayList<>();
+
